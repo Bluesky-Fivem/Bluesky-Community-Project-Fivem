f6050079553be7ace0fdecefbac16eb3e8e624a2
ItzKustom <luke15121394@hotmail.com>
2024-02-16 00:24:42 +0000

    Revert "cleaner"
    This reverts commit 8fa519704a0275112b42a8b5b4d2847d2e1e434b.

--- resources/[bluesky]/bs_sync/client/component.lua
+++ resources/[bluesky]/bs_sync/client/component.lua
@@ -1,53 +1,57 @@
-local _currentWeather = 'EXTRASUNNY'
-local _weatherState = _currentWeather
+_current = 'EXTRASUNNY'
+local _weatherState = _current
 local _timeState = 0
 local _timeOffset = 0
+local _timer = 0
 local _freezeState = false
 local _blackoutState = false
+local isTransionHappening = false
+
 local _running = false
-local _timer = 0
-local _isTransitioning = false
 
 AddEventHandler('Sync:Shared:DependencyUpdate', RetrieveComponents)
+function RetrieveComponents()
+    Callbacks = exports['bs_base']:FetchComponent('Callbacks')
+    Logger = exports['bs_base']:FetchComponent('Logger')
+    Sync = exports['bs_base']:FetchComponent('Sync')
+end
+
 AddEventHandler('Core:Shared:Ready', function()
-    exports['bs_base']:RequestDependencies('Sync', {'Callbacks', 'Logger', 'Sync'}, function(error)
-        if #error > 0 then return end
+    exports['bs_base']:RequestDependencies('Sync', {
+        'Callbacks',
+        'Logger',
+        'Sync',
+    }, function(error)
+        if #error > 0 then return; end
         RetrieveComponents()
     end)
 end)
-
 
 AddEventHandler('Characters:Client:Spawn', function()
     Sync:Start()
 end)
 
-
+RegisterNetEvent('Characters:Client:Logout')
 AddEventHandler('Characters:Client:Logout', function()
     Sync:Stop()
 end)
 
-
-AddEventHandler('Proxy:Shared:RegisterReady', function()
-    exports['bs_base']:RegisterComponent('Sync', SYNC)
-end)
-
-
-local SYNC = {
+SYNC = {
     Start = function(self)
         if _running then return end
         Callbacks:ServerCallback('Sync:GetState', {}, function(weather, blackout, base, offset, freeze)
             _running = true
-            _currentWeather = weather
+            _current = weather
             _blackoutState = blackout
             _timeState = base
             _timeOffset = offset
             _freezeState = freeze
             SetRainFxIntensity(-1.0)
+
             Logger:Trace('Sync', 'Starting Sync')
             StartSyncThreads()
         end)
     end,
-
     Stop = function(self)
         if not _running then return end
         Logger:Trace('Sync', 'Stopping Sync')
@@ -62,17 +66,50 @@ local SYNC = {
                 Citizen.Wait(5000)
             end
         end)
-    end
+    end,
 }
 
-function RetrieveComponents()
-    Callbacks = exports['bs_base']:FetchComponent('Callbacks')
-    Logger = exports['bs_base']:FetchComponent('Logger')
-    Sync = exports['bs_base']:FetchComponent('Sync')
-end
+AddEventHandler('Proxy:Shared:RegisterReady', function()
+    exports['bs_base']:RegisterComponent('Sync', SYNC)
+end)
+
+RegisterNetEvent('Sync:Client:Weather')
+AddEventHandler('Sync:Client:Weather', function(weather, override)
+    _current = weather
+end)
+
+RegisterNetEvent('Sync:Client:Time')
+AddEventHandler('Sync:Client:Time', function(base, offset)
+    _timeState = base
+    _timeOffset = offset
+
+    if _running then
+        hour = math.floor(((_timeState + _timeOffset) / 60) % 24)
+        minute = math.floor((_timeState + _timeOffset) % 60)
+        NetworkOverrideClockTime(hour, minute, 0)
+    end
+end)
+
+RegisterNetEvent('Sync:Client:Freeze')
+AddEventHandler('Sync:Client:Freeze', function(freeze)
+    _freezeState = freeze
+end)
+
+RegisterNetEvent('Sync:Client:Blackout')
+AddEventHandler('Sync:Client:Blackout', function(blackout)
+    _blackoutState = blackout
+
+    if _running then
+        SetBlackout(_blackoutState)
+    end
+end)
 
 function StartSyncThreads()
+
+
     Citizen.CreateThread(function()
+        local hour = 0
+        local minute = 0
         while _running do
             local newBaseTime = _timeState
             if GetGameTimer() - 500 > _timer then
@@ -83,8 +120,8 @@ function StartSyncThreads()
                 _timeOffset = _timeOffset + _timeState - newBaseTime
             end
             _timeState = newBaseTime
-            local hour = math.floor(((_timeState + _timeOffset) / 60) % 24)
-            local minute = math.floor((_timeState + _timeOffset) % 60)
+            hour = math.floor(((_timeState + _timeOffset) / 60) % 24)
+            minute = math.floor((_timeState + _timeOffset) % 60)
             NetworkOverrideClockTime(hour, minute, 0)
             Citizen.Wait(2000)
         end
@@ -92,16 +129,18 @@ function StartSyncThreads()
 
     Citizen.CreateThread(function()
         while _running do
-            if _weatherState ~= _currentWeather and not _isTransitioning then
-                _isTransitioning = true
-                _weatherState = _currentWeather
-                ClearOverrideWeather()
-                ClearWeatherTypePersist()
-                SetWeatherTypeOvertimePersist(_weatherState, 12.0)
-                Citizen.Wait(12000)
-                _isTransitioning = false
+            if _weatherState ~= _current then
+                if not isTransionHappening then
+                    isTransionHappening = true
+                    _weatherState = _current
+                    ClearOverrideWeather()
+                    ClearWeatherTypePersist()
+                    SetWeatherTypeOvertimePersist(_weatherState, 12.0)
+                    Citizen.Wait(12000)
+                    isTransionHappening = false
+                end
             end
-            Citizen.Wait(100)
+            Citizen.Wait(100) -- Wait 0 seconds to prevent crashing.
             SetBlackout(_blackoutState)
             SetWeatherTypePersist(_weatherState)
             SetWeatherTypeNow(_weatherState)
--- resources/[bluesky]/bs_sync/server/commands.lua
+++ resources/[bluesky]/bs_sync/server/commands.lua
@@ -1,66 +1,72 @@
 function RegisterChatCommands()
-    -- Command to freeze weather or time
     Chat:RegisterAdminCommand('freeze', function(source, args, rawCommand)
-        local arg = args[1]:lower()
-        if arg == 'weather' or arg == 'time' then
-            Sync.Set:Freeze(arg)
+        if args[1]:lower() == 'weather' or args[1]:lower() == 'time' then
+            Sync.Set:Freeze(args[1])
         else
             Chat.Send.Server:Single(source, 'Invalid Argument')
         end
     end, {
         help = 'Freeze Weather or Time',
-        params = {
-            { name = 'Type', help = 'Weather or Time' }
+        params = { {
+            name = 'Type',
+            help = 'Weather or Time'
+        }
         }
     }, 1)
 
-    -- Command to set weather
     Chat:RegisterAdminCommand('weather', function(source, args, rawCommand)
-        local arg = args[1]:upper()
-        if AvailableWeatherTypes[arg] then
-            Sync.Set:Weather(arg)
-        else
-            Chat.Send.Server:Single(source, 'Invalid Argument')
+        for _, v in pairs(AvailableWeatherTypes) do
+            if args[1]:upper() == v then
+                Sync.Set:Weather(args[1])
+                return
+            end
         end
+        Chat.Send.Server:Single(source, 'Invalid Argument')
     end, {
         help = 'Set Weather',
-        params = {
-            { name = 'Type', help = 'Weather type' }
+        params = { {
+            name = 'Type',
+            help = 'EXTRASUNNY, CLEAR, NEUTRAL, SMOG, FOGGY, OVERCAST, CLOUDS, CLEARING, RAIN, THUNDER, SNOW, BLIZZARD, SNOWLIGHT, XMAS, HALLOWEEN'
+        }
         }
     }, 1)
 
-    -- Command to set time
     Chat:RegisterAdminCommand('time', function(source, args, rawCommand)
-        local arg = args[1]:upper()
-        if AvailableTimeTypes[arg] then
-            Sync.Set:Time(arg)
-        else
-            Chat.Send.Server:Single(source, 'Invalid Argument')
+        for _, v in pairs(AvailableTimeTypes) do
+            if args[1]:upper() == v then
+                Sync.Set:Time(args[1])
+                return
+            end
         end
+        Chat.Send.Server:Single(source, 'Invalid Argument')
     end, {
         help = 'Set Time',
-        params = {
-            { name = 'Type', help = 'Time of day' }
+        params = { {
+            name = 'Type',
+            help = 'MORNING, NOON, EVENING, NIGHT'
+        }
         }
     }, 1)
 
-    -- Command to set exact time
     Chat:RegisterAdminCommand('clock', function(source, args, rawCommand)
-        local hour, minute = tonumber(args[1]), tonumber(args[2])
-        if hour and minute and hour >= 0 and hour <= 23 and minute >= 0 and minute <= 59 then
-            Sync.Set:ExactTime(hour, minute)
+        if tonumber(args[1]) ~= nil and tonumber(args[2]) ~= nil then
+            Sync.Set:ExactTime(args[1], args[2])
         else
             Chat.Send.Server:Single(source, 'Invalid Argument')
         end
     end, {
         help = 'Set Time To An Exact Hour & Minute',
-        params = {
-            { name = 'Hour', help = 'Number Between 0 - 23' },
-            { name = 'Minute', help = 'Number Between 0 - 59' }
+        params = { {
+            name = 'Hour',
+            help = 'Number Between 0 - 23'
+        },
+            {
+                name = 'Minute',
+                help = 'Number Between 0 - 59'
+            }
         }
     }, 2)
 
-    -- Command to toggle blackout
     Chat:RegisterAdminCommand('blackout', function(source, args, rawCommand)
         Sync:ToggleBlackout()
     end, {
