f9eee68b417dd114c477bda63937e367b9dbaee6
ItzKustom <luke15121394@hotmail.com>
2024-02-17 15:09:42 +0000

    Bluesky doors sql convert

--- resources/[bluesky]/bs_doors/server/doors.lua
+++ resources/[bluesky]/bs_doors/server/doors.lua
@@ -4,7 +4,6 @@ doors = {}
 -- occur as a result of overriding or extending
 AddEventHandler('Doors:Shared:DependencyUpdate', RetrieveComponents)
 function RetrieveComponents()
-    Database = exports['bs_base']:FetchComponent('Database')
     Callbacks = exports['bs_base']:FetchComponent('Callbacks')
     Logger = exports['bs_base']:FetchComponent('Logger')
     Utils = exports['bs_base']:FetchComponent('Utils')
@@ -16,7 +15,6 @@ end
 
 AddEventHandler('Core:Shared:Ready', function()
     exports['bs_base']:RequestDependencies('Doors', {
-        'Database',
         'Callbacks',
         'Logger',
         'Utils',
@@ -94,8 +92,8 @@ end
 DOORS = {
     Add = function(self, settings, source, cb)
         local nextId = 1
-        Doors:GetNext(function(next)
-            nextId = next
+        exports.oxmysql:scalar('SELECT MAX(id) FROM doors', {}, function(nextIdResult)
+            nextId = nextIdResult and (nextIdResult + 1) or 1
             local dataTable = {
                 id = nextId,
                 Coords = {
@@ -118,45 +116,35 @@ DOORS = {
                 Lockpickable = settings.lockpickable,
                 Lockdown = false
             }
-            Database.Game:insertOne({
-                collection = 'doors',
-                document = dataTable
-            }, function(success, result)
-                doors[nextId] = dataTable
-                if doors[nextId].Multi > 0 then
-                    Database.Game:updateOne({
-                        collection = 'doors',
-                        query = { id = doors[nextId].Multi },
-                        update = { ['$set'] = { Multi = nextId } }
-                    }, function(s, r)
-                        doors[Doors:GetMultiID(nextId)].Multi = nextId
+            exports.oxmysql:execute('INSERT INTO doors (id, x, y, z, h, Pitch, Yaw, DrawDistance, Public, Multi, DoorType, Model, Lock, DefaultLock, Auth, Static, Lockpickable, Lockdown) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            {nextId, settings.coords.x, settings.coords.y, settings.coords.z, settings.coords.h, settings.pitch, settings.yaw, settings.draw, settings.privacy, settings.multi, settings.type, settings.model, settings.lock, settings.defaultLock or settings.lock, settings.auth, settings.static, settings.lockpickable, 0},
+            function(success)
+                if success then
+                    doors[nextId] = dataTable
+                    if doors[nextId].Multi > 0 then
+                        exports.oxmysql:execute('UPDATE doors SET Multi = ? WHERE id = ?', {nextId, doors[nextId].Multi}, function(s)
+                            doors[Doors:GetMultiID(nextId)].Multi = nextId
+                            TriggerClientEvent('Doors:client:updateNewDoor', -1, tonumber(nextId), dataTable)
+                            TriggerClientEvent('Doors:client:adminCancelDoor', source)
+                            cb(success)
+                        end)
+                    else
                         TriggerClientEvent('Doors:client:updateNewDoor', -1, tonumber(nextId), dataTable)
                         TriggerClientEvent('Doors:client:adminCancelDoor', source)
                         cb(success)
-                        return
-                    end)
-                end
-
-                if not success then
+                    end
+                else
                     cb(success)
-                    return
                 end
-
-                TriggerClientEvent('Doors:client:updateNewDoor', -1, tonumber(nextId), dataTable)
-                TriggerClientEvent('Doors:client:adminCancelDoor', source)
-                cb(success)
-                return
             end)
         end)
     end,
+    
     Remove = function(self, door, cb)
         if doors[door].Multi > 0 then
             Doors:AdjustSettings(doors[door].Multi, 'Multi', 0, function() end)
         end
-        Database.Game:deleteOne({
-            collection = 'doors',
-            query = { id = door }
-        }, function(success, result)
+        exports.oxmysql:execute('DELETE FROM doors WHERE id = ?', {door}, function(success)
             if not success then
                 cb(success)
                 return
@@ -166,37 +154,27 @@ DOORS = {
             cb(success)
         end)
     end,
+    
     SetDefaultLocks = function(self, cb)
         Doors:Fetch(function(d)
             if Utils:GetTableLength(d) > 0 then
                 for k, v in pairs(d) do
-                    Database.Game:updateOne({
-                        collection = 'doors',
-                        query = { id = v.id },
-                        update = { ['$set'] = { Lock = v.DefaultLock } }
-                    })
+                    exports.oxmysql:execute('UPDATE doors SET Lock = ? WHERE id = ?', {v.DefaultLock, v.id})
                 end
             end
             cb(true)
         end)
     end,
+    
     SetLock = function(self, door, state, lockdown)
         if doors[door] then
             doors[door].Lock = state
             doors[door].Lockdown = lockdown and state or false
             local multi = (doors[door].Multi > 0 and doors[door].Multi or false)
             if multi then doors[Doors:GetMultiID(door)].Lock = state end
-            Database.Game:updateOne({
-                collection = 'doors',
-                query = { id = door },
-                update = { ['$set'] = { Lock = state, Lockdown = lockdown and state or false } }
-            }, function(success, result)
+            exports.oxmysql:execute('UPDATE doors SET Lock = ?, Lockdown = ? WHERE id = ?', {state, lockdown and state or false, door}, function(success)
                 if multi then
-                    Database.Game:updateOne({
-                        collection = 'doors',
-                        query = { id = multi },
-                        update = { ['$set'] = { Lock = state, Lockdown = lockdown and state or false } }
-                    }, function(s, r)
+                    exports.oxmysql:execute('UPDATE doors SET Lock = ?, Lockdown = ? WHERE id = ?', {state, lockdown and state or false, multi}, function(s)
                         TriggerClientEvent('Doors:client:updateDoor', -1, door, state)
                     end)
                 else
@@ -205,22 +183,15 @@ DOORS = {
             end)
         end
     end,
+    
     ToggleLock = function(self, door)
         if doors[door] then
             doors[door].Lock = not doors[door].Lock
             local multi = (doors[door].Multi > 0 and doors[door].Multi or false)
             if multi then doors[Doors:GetMultiID(door)].Lock = not doors[Doors:GetMultiID(door)].Lock end
-            Database.Game:updateOne({
-                collection = 'doors',
-                query = { id = door },
-                update = { ['$set'] = { Lock = not doors[door].Lock } }
-            }, function(success, result)
+            exports.oxmysql:execute('UPDATE doors SET Lock = ? WHERE id = ?', {not doors[door].Lock, door}, function(success)
                 if multi then
-                    Database.Game:updateOne({
-                        collection = 'doors',
-                        query = { id = multi },
-                        update = { ['$set'] = { Lock = not doors[Doors:GetMultiID(door)].Lock } }
-                    }, function(s, r)
+                    exports.oxmysql:execute('UPDATE doors SET Lock = ? WHERE id = ?', {not doors[Doors:GetMultiID(door)].Lock, multi}, function(s)
                         TriggerClientEvent('Doors:client:updateDoor', -1, door, not doors[Doors:GetMultiID(door)].Lock)
                     end)
                 else
@@ -229,63 +200,64 @@ DOORS = {
             end)
         end
     end,
+    
     AdjustSettings = function(self, door, setting, value, cb)
-        Database.Game:updateOne({
-            collection = 'doors',
-            query = { id = door },
-            update = { ['$set'] = { [setting] = value } }
-        }, function(success, result)
-            if doors[door].Multi > 0 then
-                Database.Game:updateOne({
-                    collection = 'doors',
-                    query = { id = doors[door].Multi },
-                    update = { ['$set'] = { [setting] = value } }
-                }, function(s, r)
-                    local multiIndex = Doors:GetMultiID(door)
-                    doors[multiIndex][setting] = value
-                    TriggerClientEvent('Doors:client:updateSettings', -1, multiIndex, setting, value)
-                end)
-            end
-            doors[door][setting] = value
-            TriggerClientEvent('Doors:client:updateSettings', -1, door, setting, value)
-            cb(success)
-        end)
-    end,
-    Fetch = function(self, cb)
-        Database.Game:find({
-            collection = 'doors',
-            query = {}
-        }, function(success, d)
-            cb(d)
-        end)
-    end,
-    GetNext = function(self, cb)
-        local nextId = Database.Game:updateOne({
-            collection = 'counters',
-            update = { ['$inc'] = { next = 1 } },
-            query = { type = 'doors' },
-            options = { upsert = true }
-        }, function(success, wtf)
-            Database.Game:findOne({
-                collection = 'counters',
-                query = { type = 'doors' }
-            }, function(success, v)
-                cb(tonumber(v[1].next))
+        exports.oxmysql:transaction(function(callback)
+            exports.oxmysql:execute('UPDATE doors SET ' .. setting .. ' = ? WHERE id = ?', {value, door}, function(success)
+                if not success then
+                    cb(success)
+                    return
+                end
+                if doors[door].Multi > 0 then
+                    exports.oxmysql:execute('UPDATE doors SET ' .. setting .. ' = ? WHERE id = ?', {value, doors[door].Multi}, function(s)
+                        local multiIndex = Doors:GetMultiID(door)
+                        doors[multiIndex][setting] = value
+                        TriggerClientEvent('Doors:client:updateSettings', -1, multiIndex, setting, value)
+                        callback(success)
+                    end)
+                else
+                    doors[door][setting] = value
+                    TriggerClientEvent('Doors:client:updateSettings', -1, door, setting, value)
+                    callback(success)
+                end
             end)
+        end, function(finalSuccess)
+            cb(finalSuccess)
         end)
     end,
+    
+    Fetch = function(self, cb)
+        exports.oxmysql:execute('SELECT * FROM doors', {}, function(result)
+            cb(result)
+        end)
+    end,
+
+    GetNext = function(self, cb)
+        exports.oxmysql:transaction(function(callback)
+            exports.oxmysql:execute('INSERT INTO counters (type, next) VALUES (?, 1) ON DUPLICATE KEY UPDATE next = LAST_INSERT_ID(next + 1)', {'doors'}, function(success)
+                if not success then
+                    cb(nil)
+                    return
+                end
+                exports.oxmysql:scalar('SELECT LAST_INSERT_ID()', {}, function(nextId)
+                    callback(nextId)
+                end)
+            end)
+        end, function(finalSuccess, insertedId)
+            cb(insertedId)
+        end)
+    end,
+    
     GetMultiID = function(self, door)
         if doors[door] then
             local multi = doors[door].Multi
             if multi > 0 then
-                for k, v in pairs(doors) do
-                    if v.id == multi then
-                        return k
-                    end
-                end
+                exports.oxmysql:scalar('SELECT id FROM doors WHERE id = ?', {multi}, function(multiId)
+                    return multiId
+                end)
             end
-
-            return 0
         end
+        return 0
     end
+    
 }
