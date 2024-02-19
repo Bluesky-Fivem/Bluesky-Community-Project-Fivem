d0ae08164c1e03e8a1404af54042d09082577d97
ItzKustom <luke15121394@hotmail.com>
2024-02-19 23:39:57 +0000

    updated sql for wallet cash

--- SQL/bs.sql
+++ SQL/bs.sql
@@ -45,12 +45,31 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `HP` int(11) DEFAULT NULL,
   `skin` longtext DEFAULT NULL,
   `Admin` varchar(50) DEFAULT 'user',
+  `inventory` longtext DEFAULT NULL,
+  `Cash` int(255) DEFAULT 5000,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table blueskysql.characters: ~1 rows (approximately)
-INSERT IGNORE INTO `characters` (`_id`, `User`, `First`, `Last`, `Phone`, `Gender`, `Bio`, `DOB`, `LastPlayed`, `Job`, `Armor`, `HP`, `skin`, `Admin`) VALUES
-	(54, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b', 'dddddddddd', 'ddddddddddd', '6284647432', '0', NULL, '1990-12-31T23:59:59.000Z', 1708129715000, '{"grade":{"level":0,"label":"Unemployed","id":0},"workplace":{"label":"Unemployed","id":0},"salary":10,"job":"unemployed","label":"Unemployed"}', 0, 120, '{"customization":{"components":{"shoes":{"componentId":6,"paletteId":0,"textureId":0,"drawableId":0},"bag":{"componentId":5,"paletteId":0,"textureId":0,"drawableId":0},"mask":{"componentId":1,"paletteId":0,"textureId":0,"drawableId":0},"kevlar":{"componentId":9,"paletteId":0,"textureId":0,"drawableId":0},"torso2":{"componentId":11,"paletteId":0,"textureId":0,"drawableId":0},"undershirt":{"componentId":8,"paletteId":0,"textureId":0,"drawableId":0},"leg":{"componentId":4,"paletteId":0,"textureId":0,"drawableId":0},"face":{"componentId":0,"paletteId":0,"textureId":0,"drawableId":0},"accessory":{"componentId":7,"paletteId":0,"textureId":0,"drawableId":0},"torso":{"componentId":3,"paletteId":0,"textureId":0,"drawableId":0},"hair":{"componentId":2,"paletteId":0,"textureId":0,"drawableId":0},"badge":{"componentId":10,"paletteId":0,"textureId":0,"drawableId":0}},"face":{"face1":{"index":0,"texture":0,"mix":50.0},"face2":{"index":0,"texture":0,"mix":50.0},"features":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},"props":{"watch":{"componentId":6,"disabled":true,"textureId":0,"drawableId":0},"bracelet":{"componentId":7,"disabled":true,"textureId":0,"drawableId":0},"glass":{"componentId":1,"disabled":true,"textureId":0,"drawableId":0},"hat":{"componentId":0,"disabled":true,"textureId":0,"drawableId":0},"ear":{"componentId":2,"disabled":true,"textureId":0,"drawableId":0}},"overlay":{"eyebrows":{"id":2,"opacity":100.0,"index":0,"disabled":true},"freckles":{"id":9,"opacity":100.0,"index":0,"disabled":true},"blemish":{"id":0,"opacity":100.0,"index":0,"disabled":true},"complexion":{"id":6,"opacity":100.0,"index":0,"disabled":true},"bodyblemish":{"id":11,"opacity":100.0,"index":0,"disabled":true},"makeup":{"id":4,"opacity":100.0,"index":0,"disabled":true},"ageing":{"id":3,"opacity":100.0,"index":0,"disabled":true},"sundamage":{"id":7,"opacity":100.0,"index":0,"disabled":true},"facialhair":{"id":1,"opacity":100.0,"index":0,"disabled":true},"blush":{"id":5,"opacity":100.0,"index":0,"disabled":true},"lipstick":{"id":8,"opacity":100.0,"index":0,"disabled":true},"addbodyblemish":{"id":12,"opacity":100.0,"index":0,"disabled":true},"chesthair":{"id":10,"opacity":100.0,"index":0,"disabled":true}},"colors":{"facialhair":{"color2":{"rgb":"rgb(0, 0, 0)","index":0},"color1":{"rgb":"rgb(0, 0, 0)","index":0}},"chesthair":{"color2":{"rgb":"rgb(0, 0, 0)","index":0},"color1":{"rgb":"rgb(0, 0, 0)","index":0}},"hair":{"color2":{"rgb":"rgb(0, 0, 0)","index":0},"color1":{"rgb":"rgb(0, 0, 0)","index":0}}}},"model":""}', 'dev');
+-- Dumping data for table blueskysql.characters: ~3 rows (approximately)
+INSERT IGNORE INTO `characters` (`_id`, `User`, `First`, `Last`, `Phone`, `Gender`, `Bio`, `DOB`, `LastPlayed`, `Job`, `Armor`, `HP`, `skin`, `Admin`, `inventory`, `Cash`) VALUES
+	(54, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b', 'dddddddddd', 'ddddddddddd', '6284647432', '0', NULL, '1990-12-31T23:59:59.000Z', 1708385856000, '{"salary":10,"grade":{"level":0,"label":"Unemployed","id":0},"label":"Unemployed","workplace":{"id":0,"label":"Unemployed"},"job":"unemployed"}', 0, 200, '{"customization":{"components":{"shoes":{"componentId":6,"paletteId":0,"textureId":0,"drawableId":0},"bag":{"componentId":5,"paletteId":0,"textureId":0,"drawableId":0},"mask":{"componentId":1,"paletteId":0,"textureId":0,"drawableId":0},"kevlar":{"componentId":9,"paletteId":0,"textureId":0,"drawableId":0},"torso2":{"componentId":11,"paletteId":0,"textureId":0,"drawableId":0},"undershirt":{"componentId":8,"paletteId":0,"textureId":0,"drawableId":0},"leg":{"componentId":4,"paletteId":0,"textureId":0,"drawableId":0},"face":{"componentId":0,"paletteId":0,"textureId":0,"drawableId":0},"accessory":{"componentId":7,"paletteId":0,"textureId":0,"drawableId":0},"torso":{"componentId":3,"paletteId":0,"textureId":0,"drawableId":0},"hair":{"componentId":2,"paletteId":0,"textureId":0,"drawableId":0},"badge":{"componentId":10,"paletteId":0,"textureId":0,"drawableId":0}},"face":{"face1":{"index":0,"texture":0,"mix":50.0},"face2":{"index":0,"texture":0,"mix":50.0},"features":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},"props":{"watch":{"componentId":6,"disabled":true,"textureId":0,"drawableId":0},"bracelet":{"componentId":7,"disabled":true,"textureId":0,"drawableId":0},"glass":{"componentId":1,"disabled":true,"textureId":0,"drawableId":0},"hat":{"componentId":0,"disabled":true,"textureId":0,"drawableId":0},"ear":{"componentId":2,"disabled":true,"textureId":0,"drawableId":0}},"overlay":{"eyebrows":{"id":2,"opacity":100.0,"index":0,"disabled":true},"freckles":{"id":9,"opacity":100.0,"index":0,"disabled":true},"blemish":{"id":0,"opacity":100.0,"index":0,"disabled":true},"complexion":{"id":6,"opacity":100.0,"index":0,"disabled":true},"bodyblemish":{"id":11,"opacity":100.0,"index":0,"disabled":true},"makeup":{"id":4,"opacity":100.0,"index":0,"disabled":true},"ageing":{"id":3,"opacity":100.0,"index":0,"disabled":true},"sundamage":{"id":7,"opacity":100.0,"index":0,"disabled":true},"facialhair":{"id":1,"opacity":100.0,"index":0,"disabled":true},"blush":{"id":5,"opacity":100.0,"index":0,"disabled":true},"lipstick":{"id":8,"opacity":100.0,"index":0,"disabled":true},"addbodyblemish":{"id":12,"opacity":100.0,"index":0,"disabled":true},"chesthair":{"id":10,"opacity":100.0,"index":0,"disabled":true}},"colors":{"facialhair":{"color2":{"rgb":"rgb(0, 0, 0)","index":0},"color1":{"rgb":"rgb(0, 0, 0)","index":0}},"chesthair":{"color2":{"rgb":"rgb(0, 0, 0)","index":0},"color1":{"rgb":"rgb(0, 0, 0)","index":0}},"hair":{"color2":{"rgb":"rgb(0, 0, 0)","index":0},"color1":{"rgb":"rgb(0, 0, 0)","index":0}}}},"model":""}', 'dev', NULL, 5000),
+	(60, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b', 'sssssssssss', 'ssssssssssssssssssss', '6285682218', '0', NULL, '2000-01-01T00:00:00.000Z', 1708385856000, '{"grade":{"level":0,"id":0,"label":"Unemployed"},"label":"Unemployed","workplace":{"label":"Unemployed","id":0},"salary":10,"job":"unemployed"}', 100, 200, NULL, 'user', NULL, 5000),
+	(61, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b', 'wwwwwwwwwwwww', 'wwwwwwwwwwwwwwwww', '4157392653', '0', NULL, '1990-12-31T23:59:59.000Z', 1708385856000, '{"salary":10,"label":"Unemployed","workplace":{"id":0,"label":"Unemployed"},"job":"unemployed","grade":{"id":0,"level":0,"label":"Unemployed"}}', 100, 200, NULL, 'user', NULL, 5000);
+
+-- Dumping structure for table blueskysql.character_emails
+CREATE TABLE IF NOT EXISTS `character_emails` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` int(11) DEFAULT NULL,
+  `sender` varchar(255) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `body` text DEFAULT NULL,
+  `unread` tinyint(1) DEFAULT NULL,
+  `flags` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table blueskysql.character_emails: ~0 rows (approximately)
 
 -- Dumping structure for table blueskysql.counters
 CREATE TABLE IF NOT EXISTS `counters` (
@@ -59,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `counters` (
   PRIMARY KEY (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table blueskysql.counters: ~1 rows (approximately)
+-- Dumping data for table blueskysql.counters: ~0 rows (approximately)
 INSERT IGNORE INTO `counters` (`type`, `next`) VALUES
 	('doors', 1);
 
@@ -81,16 +100,16 @@ CREATE TABLE IF NOT EXISTS `doors` (
   `Pitch` float NOT NULL,
   `Yaw` float NOT NULL,
   `DrawDistance` float NOT NULL,
-  `Public` tinyint(1) NOT NULL,
+  `Public` tinyint(1) DEFAULT NULL,
   `Multi` int(11) NOT NULL,
   `DoorType` varchar(255) NOT NULL,
   `Model` varchar(255) NOT NULL,
-  `DefaultLock` tinyint(1) NOT NULL,
-  `Auth` varchar(255) NOT NULL,
-  `Static` tinyint(1) NOT NULL,
-  `Lockpickable` tinyint(1) NOT NULL,
-  `Lockdown` tinyint(1) NOT NULL,
   `Lock` tinyint(4) NOT NULL,
+  `DefaultLock` tinyint(1) NOT NULL,
+  `Auth` tinyint(1) DEFAULT NULL,
+  `Static` tinyint(1) DEFAULT NULL,
+  `Lockpickable` tinyint(1) DEFAULT NULL,
+  `Lockdown` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -101,17 +120,16 @@ CREATE TABLE IF NOT EXISTS `locations` (
   `_id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
   `Type` varchar(255) DEFAULT NULL,
-  `Coords` varchar(255) DEFAULT NULL,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
   `h` float DEFAULT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table blueskysql.locations: ~0 rows (approximately)
-INSERT IGNORE INTO `locations` (`_id`, `Name`, `Type`, `Coords`, `x`, `y`, `z`, `h`) VALUES
-	(1, 'mrpd', 'spawn', '1', 1000, 1000, 1000, 1000);
+-- Dumping data for table blueskysql.locations: ~1 rows (approximately)
+INSERT IGNORE INTO `locations` (`_id`, `Name`, `Type`, `x`, `y`, `z`, `h`) VALUES
+	(2, 'MRPD', 'spawn', 429.218, -982.734, 30.6952, 85.0394);
 
 -- Dumping structure for table blueskysql.logs
 CREATE TABLE IF NOT EXISTS `logs` (
@@ -125,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `logs` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table blueskysql.logs: ~13 rows (approximately)
+-- Dumping data for table blueskysql.logs: ~12 rows (approximately)
 INSERT IGNORE INTO `logs` (`id`, `level`, `log`, `date`, `server`, `component`, `extra`) VALUES
 	(1, 0, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b attempted to use an admin command but failed Admin Validation.', 1708119952, NULL, 'Commands', NULL),
 	(2, 0, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b attempted to use an admin command but failed Admin Validation.', 1708120000, NULL, 'Commands', NULL),
@@ -141,6 +159,17 @@ INSERT IGNORE INTO `logs` (`id`, `level`, `log`, `date`, `server`, `component`, 
 	(12, 0, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b attempted to use an admin command but failed Admin Validation.', 1708122334, NULL, 'Commands', NULL),
 	(13, 0, 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b attempted to use an admin command but failed Admin Validation.', 1708122335, NULL, 'Commands', NULL);
 
+-- Dumping structure for table blueskysql.ox_inventory
+CREATE TABLE IF NOT EXISTS `ox_inventory` (
+  `owner` varchar(60) DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `data` longtext DEFAULT NULL,
+  `lastupdated` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  UNIQUE KEY `owner` (`owner`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table blueskysql.ox_inventory: ~0 rows (approximately)
+
 -- Dumping structure for table blueskysql.peds
 CREATE TABLE IF NOT EXISTS `peds` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -152,6 +181,29 @@ CREATE TABLE IF NOT EXISTS `peds` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Dumping data for table blueskysql.peds: ~0 rows (approximately)
+
+-- Dumping structure for table blueskysql.phone_contacts
+CREATE TABLE IF NOT EXISTS `phone_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `character_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `color` varchar(20) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `favorite` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table blueskysql.phone_contacts: ~0 rows (approximately)
+
+-- Dumping structure for table blueskysql.player_vehicles
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) DEFAULT NULL,
+  `glovebox` longtext DEFAULT NULL,
+  `trunk` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table blueskysql.player_vehicles: ~0 rows (approximately)
 
 -- Dumping structure for table blueskysql.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -168,15 +220,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table blueskysql.users: ~2 rows (approximately)
 INSERT IGNORE INTO `users` (`id`, `name`, `sid`, `identifier`, `priority`, `roles`, `group`) VALUES
 	(5, 'luke1', 'e:a7602f66b0a43e8efdc232765bbb0e2b63bab26b@blue.sky', 'license:a7602f66b0a43e8efdc232765bbb0e2b63bab26b', 0, 'dev', 'user');
-
--- Dumping structure for table blueskysql.wallets
-CREATE TABLE IF NOT EXISTS `wallets` (
-  `Char` varchar(50) NOT NULL,
-  `Cash` int(11) NOT NULL DEFAULT 5000,
-  PRIMARY KEY (`Char`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- Dumping data for table blueskysql.wallets: ~0 rows (approximately)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
