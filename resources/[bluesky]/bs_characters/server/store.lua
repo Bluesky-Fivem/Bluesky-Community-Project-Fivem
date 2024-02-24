449e825d124d3d84a286b1107933e33ffe090ae7
ItzKustom <152270436+PainCakezZ@users.noreply.github.com>
2024-02-24 02:07:04 +0000

    Fixed the store

--- resources/[bluesky]/bs_characters/server/store.lua
+++ resources/[bluesky]/bs_characters/server/store.lua
@@ -1,4 +1,4 @@
-local _noUpdate = { 'Source', 'User', '_id', 'ID', 'First', 'Last', 'Phone', 'DOB', 'Gender' }
+local _noUpdate = { 'Source', 'User', '_id',  'First', 'Last', 'Phone', 'DOB', 'Gender' }
 
 
 function StoreData(userId, char)
@@ -9,16 +9,20 @@ function StoreData(userId, char)
     end
 
     local ped = GetPlayerPed(char:GetData('Source'))
+    local id = data.ID
     data.HP = GetEntityHealth(ped)
     data.Armor = GetPedArmour(ped)
     data.LastPlayed = os.time() * 1000
 
-    MySQL.Async.execute("UPDATE characters SET HP = @hp, Armor = @armor, LastPlayed = @lastPlayed WHERE User = @userId", {
+    exports.oxmysql:update("UPDATE characters SET HP = @hp, Armor = @armor, LastPlayed = @lastPlayed, Job = @job WHERE _id = @userId", {
         ['@hp'] = data.HP,
         ['@armor'] = data.Armor,
-        ['@lastPlayed'] = data.LastPlayed,
-        ['@userId'] = userId,
+        ['@lastPlayed'] = tonumber(data.LastPlayed),
+        ['@userId'] = id,
+        ['@job'] = json.encode(data.Job)
     }, function(rowsChanged)
+        print(json.encode(rowsChanged))
+        --Logger:Trace('Characters', 'Storing Character Data', { console = true })
         -- Handle success or failure here if needed
         -- rowsChanged contains the number of rows affected by the query
     end)
@@ -26,10 +30,10 @@ end
 
 
 Citizen.CreateThread(function()
-    while Fetch == nil or Database == nil do Citizen.Wait(1000) end
+    while Fetch == nil  do Citizen.Wait(1000) end
 
     while true do
-        Citizen.Wait(180000)
+        Citizen.Wait(10000)
         Logger:Trace('Characters', 'Storing Character Data', { console = true })
         for k, v in pairs(Fetch:All()) do
             local char = v:GetData('Character')
