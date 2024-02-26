aa48ab6c1640dbf4d4da131b4a8a04c37a367ddd
ItzKustom <152270436+PainCakezZ@users.noreply.github.com>
2024-02-26 01:40:10 +0000

    RPC on component

---
+++ resources/[bluesky]/bs_rpc/client/cl_main.lua
@@ -0,0 +1,97 @@
+AddEventHandler('RPC:Shared:DependencyUpdate', RetrieveComponents)
+function RetrieveComponents()
+    RPC = exports['bs_base']:FetchComponent('RPC')
+end
+
+AddEventHandler('Core:Shared:Ready', function()
+    exports['bs_base']:RequestDependencies('RPC', {
+        'RPC'
+    }, function(error)  
+        if #error > 0 then return; end
+        RetrieveComponents()
+    end)
+end)
+
+
+AddEventHandler('Proxy:Shared:RegisterReady', function()
+    exports['bs_base']:RegisterComponent('RPC', remoteCalls)
+end)
+
+local res, promises, functions, callIden = GetCurrentResourceName(), {}, {}, 0
+remoteCalls = {
+    execute = function(name, ...)
+        local callId, solved = callIden, false
+        callIden = callIden + 1
+    
+        promises[callId] = promise:new()
+        TriggerServerEvent('bs_rpc:cl_request', res, name, callId, paramPacker(...))
+    
+        Citizen.SetTimeout(20000, function()
+            if not solved then
+                promises[callId]:resolve({nil})
+            end
+        end)
+        local response = Citizen.Await(promises[callId])
+        solved = true
+        clearPromise(callId)
+    
+        return paramUnpacker(response)
+    end
+}
+
+paramPacker = function(...)
+    local params, pack = {...}, {}
+
+    for i = 1, 15, 1 do
+        pack[i] = {param = params[i]}
+    end
+    return pack
+end
+
+paramUnpacker = function(params, index)
+    local idx = index or 1
+
+    if idx <= #params then return params[idx]['param'], paramPacker(params, idx + 1) end
+end
+
+unpacker = function(params, index)
+    local idx = index or 1
+
+    if idx <= 15 then return params[idx], unpacker(params, idx + 1) end
+end
+
+clearPromise = function(callId)
+    Citizen.SetTimeout(5000, function()
+        promises[callId] = nil
+    end)
+end
+
+remoteCalls.execute = function(name, ...)
+    local callId, solved = callIden, false
+    callIden = callIden + 1
+
+    promises[callId] = promise:new()
+    TriggerServerEvent('bs_rpc:cl_request', res, name, callId, paramPacker(...))
+
+    Citizen.SetTimeout(20000, function()
+        if not solved then
+            promises[callId]:resolve({nil})
+        end
+    end)
+    local response = Citizen.Await(promises[callId])
+    solved = true
+    clearPromise(callId)
+
+    return paramUnpacker(response)
+end
+
+RegisterNetEvent('bs_rpc:cl_response')
+AddEventHandler('bs_rpc:cl_response', function(origin, callId, response)
+    if res == origin and promises[callId] then
+        promises[callId]:resolve(response)
+    end
+end)
+
+function test(name, ...)
+    print(...)
+end
---
+++ resources/[bluesky]/bs_rpc/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'common'
+
+
+description 'BlueSky Data Fetch'
+author 'PainCakez'
+version '1.0'
+
+client_scripts {'client/*.lua'}
+server_scripts {'server/*.lua'}
