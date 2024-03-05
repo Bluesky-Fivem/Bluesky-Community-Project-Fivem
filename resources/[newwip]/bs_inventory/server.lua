53369239e98c66a272cf295f5b2cf11118d708a9
ItzKustom <152270436+PainCakezZ@users.noreply.github.com>
2024-03-05 21:22:27 +0000

    Admin Command Give Item :hammer:

--- resources/[newwip]/bs_inventory/server.lua
+++ resources/[newwip]/bs_inventory/server.lua
@@ -3,17 +3,20 @@ AddEventHandler('Inventory:Shared:DependencyUpdate', RetrieveComponents)
 function RetrieveComponents()
     Callbacks = exports['bs_base']:FetchComponent('Callbacks')
     Wallet = exports['bs_base']:FetchComponent('Wallet')
+    Chat = exports['bs_base']:FetchComponent("Chat")
 
 end
 
 AddEventHandler('Core:Shared:Ready', function()
     exports['bs_base']:RequestDependencies('Inventory', {
         'Callbacks',
-        'Wallet'
+        'Wallet',
+        'Chat'
     }, function(error)
         if #error > 0 then return end -- Do something to handle if not all dependencies loaded
         RetrieveComponents()
         RegisterCallbacks()
+        RegisterChatCommands()
     end)
 end)
 
@@ -27,8 +30,22 @@ function RegisterCallbacks()
         }
         cb(someData)
     end)
+
 end
 
+function RegisterChatCommands()
+    Chat:RegisterAdminCommand("giveitem", function(source, args, rawCommand)
+        TriggerClientEvent('player:receiveItem', source, args[1], args[2])
+    end, {
+        help = "Give Yourself A Item",
+        params = {
+            {
+                name = "Item Name",
+                help = "Amount To Give",
+            },
+        },
+    },2)
+end
 
 RegisterServerEvent("cash:remove")
 AddEventHandler("cash:remove", function(pSource, pAmount)
